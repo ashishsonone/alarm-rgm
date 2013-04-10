a5078c3cbe56f69c9d9d64573447e2f26180a340
Dhanesh Kumar <dhanesh_kr@iitb.ac.in>
2013-04-10 19:25:19 +0530

    modified python script to generate whole thing in a html file added

--- cs296_base_code/scripts/g13_gen_html.py
+++ cs296_base_code/scripts/g13_gen_html.py
@@ -9,38 +9,58 @@ def parser(subsection):
 	
 def itemrep(content):
 	content = re.sub(r'\\\\',"\n", content)
-	content=content.replace("\\","@")
-	content = re.sub(r'@begin{itemize}',"<ul>",content)
-	content = re.sub(r'@end{itemize}',"</ul>", content)
-	content = re.sub(r'@item',"<li>", content)
-	content = re.sub(r'(@textbf[ ]*{)(.*)(})', r'<b>\2</b>', content)
-#	content = imgtag(content)
+	content=content.replace("\\","~")
+	content = re.sub(r'~begin[ ]*{itemize}',"\t<ul>",content)
+	content = re.sub(r'~end {document}',"",content)
+	content = re.sub(r'~end[ ]*{itemize}',"\t</ul>", content)
+	content = re.sub(r'~item',"\t\t<li>", content)
+	content = re.sub(r'(~textbf[ ]*{)(.*)(})', r'<b>\2</b>', content)
+	content = re.sub(r'(~texttt{)(.*?)(})', r'<b>\2</b>', content)
+	content = re.sub(r'~_',"_", content)
+	content = imgtag(content)
 	return content
 
 
-def imgtag(cont):
-#	cont=re.sub(r'\b',"$",cont)
-	content=re.findall(r'$egin{figure}',cont)
-	content=re.findall(r'{minipage}',cont)
-	print(content,'\n')
-	for i in content:
-#		print(i)
-#		continue
-		p=[]
-		t=[]
-		paths = re.findall( 'h]{[A-Za-z0-9_/.]+', i, re.MULTILINE )
-		for dirtyLoc in paths:
-			loc = "../" + dirtyLoc[3:]
-			p.append(loc)
-		titles = re.findall( 'on{[A-Za-z0-9()\-]+', i, re.MULTILINE )
-		for dirtyTitle in titles:
-			label = dirtyTitle[3:]
-			t.append(label)
+def imgtag(cont):	
+	#content1=re.findall(r'~begin{figure}',cont)
+	content2=re.findall(r'(~begin{figure})(.*)(end{figure})',cont,re.DOTALL)
+	#print(content2,'\n')
+	
+	for j in content2:
+		print("===================================================================================",'\n')
+		print(j[1],'\n')
+		
+		content=re.findall(r'(includegraphics)(.*?)(caption{)(.*?)(})',j[1],re.DOTALL)
+		print("------------------------------------------------------")
+		#print(content,'\n')
 		ste=""
-		for i in range(len(p)):
-			ste += "<img src=" + p[i] + ", height=\"100pt\>\n"
-			ste += "<p>"+ t[i]+"</p>\n"
-		cont= cont.replace(i,ste)
+		for i in content:
+			p=[]
+			t=[]
+			paths = re.findall( 'h]{[A-Za-z0-9_/.]+',i[1], re.MULTILINE )
+			#print(i[1])		
+			
+			for dirtyLoc in paths:
+				loc = "../" + dirtyLoc[3:]
+				print(loc)			
+				p.append(loc)
+				mm=i[2]+i[3]+i[4]
+			#	print(mm)
+			titles = re.findall( '(on{[A-Za-z0-9()\- \,]+)(})',mm, re.MULTILINE )
+			#print(titles)
+			
+			for dirtyTitle in titles:
+				label = dirtyTitle[0][3:]
+				print(label,'\n')
+				t.append(label)
+			
+		
+			for k in range(len(t)):
+				ste += """<p align=\"center\"><img src=\"""" + p[k] +".png"+ """ " height="400px " \></p>"""
+				ste += "<p align=\"center\"><i><b>"+ t[k]+"</b></i></p>\n"
+			
+		replace = j[0] + j[1] + j[2] 
+		cont= cont.replace(replace,ste)
 	return cont
 
 
@@ -50,8 +70,8 @@ import os
 import re
 import sys
 
-texfile = "./doc/g13_prof_report.tex"
-output = "./doc/g13_lab09_report.html"
+texfile = "g13_prof_report.tex"
+output = "g13_lab09_report.html"
 
 if os.path.exists( texfile ):
 	text = open( texfile, 'r' )
@@ -92,11 +112,11 @@ for subc in sections[1:] :
 
 HTML=""
 for S in SL:
-	HTML+="<h1>"+S[0][0]+"</h1>\n"
-	HTML+="<p>" + itemrep(S[0][1])+ "</p\n"
+	HTML+="\n<h1>"+S[0][0]+"</h1>\n"
+	HTML+="\n<p>" + itemrep(S[0][1])+ "\n</p>\n"
 	for sub in S[1]:
-		HTML+="<h2>"+ sub[0]+"</h2>\n"
-		HTML+="<p>" + itemrep(sub[1]) + "</p\n"
+		HTML+="\n<h2>"+ sub[0]+"</h2>\n"
+		HTML+="\t<p>" + itemrep(sub[1]) + "\n\t</p>\n"
 
 #print(HTML)
 
@@ -104,4 +124,6 @@ fpout.write(HTML)
 fpout.write(endHtml)
 text.close()
 fpout.close()
+
+
 
